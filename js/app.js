2d509b3d9becad4fe0fd8d7114995ef944aed8df
Diane <yunfeiyue@me.com>
2016-11-06 15:46:58 -0800

    add car move features

--- js/app.js
+++ js/app.js
@@ -16,8 +16,71 @@ $('.start').on('click', function(){
 
 });
 
+function Player(id, keycode, position, carColor){
+  this.id = id;
+  this.keycode = keycode;
+  this.position = position;
+  this.carColor = carColor;
+}
+
+
+function startGame(){
+  var playerList = [];
+  var player1 = new Player('car1', 0, 'blue');
+  var player2 = new Player('car2', 0, 'red');
+  playerList.push(player1);
+  playerList.push(player2);
+
+
+  $(document).keydown(function(e){
+      e.preventDefault();
+      var position1 =$('#car1').position();
+      var position2 =$('#car2').position();
+
+      switch (e.keyCode){
+        case 37:    //car1 move left
+            if(position1.left>0){
+              $("#car1").css('left', position1.left - 20 +'px');
+            }
+            break;
+        case 38:    //car1 move up
+            $("#car1").css('top', position1.top - 20 +'px');
+            break;
+        case 39:    //car1 move right
+            $("#car1").css('left', position1.left + 20 +'px');
+            break;
+        case 40:    //car1 move down
+            if(position1.top<0){
+              $("#car1").css('top', position1.top + 20 +'px');
+            }
+            break;
+        case 65:    //car1 move left "character: a"
+            if(position2.left>0){
+              $("#car2").css('left', position2.left - 20 +'px');
+            }
+            break;
+        case 87:    //car2 move up "character: w"
+            $("#car2").css('top', position2.top - 20 +'px');
+            break;
+        case 68:    //car2 move right "character: d"
+            $("#car2").css('left', position2.left + 20 +'px');
+            break;
+        case 88:    //car2 move down "character: z"
+            if(position2.top<0){
+              $("#car2").css('top', position2.top + 20 +'px');
+            }
+            break;
+      }
+  });
+
+
+
+}
 $(document).ready(function() {
 //   // all code to manipulate the DOM
 //   // goes inside this function
+  startGame();
+
+//
 
 });
